ba1f6ab9bb1209566c22fe3d5e11dbff80973771
Ramen <karna.ramenk@gmail.com>
2020-07-12 09:55:14 +0530

    add check if a player move is valid

--- game.py
+++ game.py
@@ -19,10 +19,21 @@ while playing:
     while game_status == "ongoing":
         show(game_board)
 
-        # player move
+       # player move
         current_player = next(players)
-        game_board = change(game_board, current_player)
-
+        valid_move = False
+        while not valid_move:
+            try: 
+                game_board = change(game_board, current_player)
+                valid_move = True
+                
+            except Exception as e:
+                print("\n")
+                print("Something went wrong:", e)
+                print("Hint: The chosen position must be available on the board.")
+                print("Hint: The values must be separated by a comma e.g. '0, 1'.")
+                print("Try again!", "\n")
+                
         # stopping condition for while-loop
         game_status = determine_game_status(game_board)
         if game_status != "ongoing":
